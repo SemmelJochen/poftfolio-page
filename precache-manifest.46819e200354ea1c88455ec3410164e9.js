d70ad81cecee847c2b3ea7b7678591793a51ec2b
FeedThePiano <jokup@t-online.de>
2020-03-03 15:24:20 +0100

    Updates

---
+++ precache-manifest.46819e200354ea1c88455ec3410164e9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13054cba9e6938c2825d2d9700fa62a",
+    "url": "/poftfolio-page/index.html"
+  },
+  {
+    "revision": "b46babafc5d5432b7344",
+    "url": "/poftfolio-page/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8f8c424bae97bd236787",
+    "url": "/poftfolio-page/static/js/2.bbf8806c.chunk.js"
+  },
+  {
+    "revision": "ff5f7d0cf2a83013a842ccae1b677ee1",
+    "url": "/poftfolio-page/static/js/2.bbf8806c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b46babafc5d5432b7344",
+    "url": "/poftfolio-page/static/js/main.180b5cf4.chunk.js"
+  },
+  {
+    "revision": "75de0c449d5d6be669e5",
+    "url": "/poftfolio-page/static/js/runtime-main.447cfd11.js"
+  },
+  {
+    "revision": "fdada7e8bd4292b741709a1cb2bf956c",
+    "url": "/poftfolio-page/static/media/IMG1-2.fdada7e8.jpg"
+  },
+  {
+    "revision": "3a1c75496c506551f99689828ab6123c",
+    "url": "/poftfolio-page/static/media/IMG1.3a1c7549.jpg"
+  },
+  {
+    "revision": "2d5e8016e1021c3bd6f4dce5a8b1d3fd",
+    "url": "/poftfolio-page/static/media/IMG10.2d5e8016.jpg"
+  },
+  {
+    "revision": "9c13e298abf207bf8eabee57dc03c139",
+    "url": "/poftfolio-page/static/media/IMG11.9c13e298.jpg"
+  },
+  {
+    "revision": "752e875428949c9012bc13e9b2e4d065",
+    "url": "/poftfolio-page/static/media/IMG12.752e8754.jpg"
+  },
+  {
+    "revision": "e9704e6e5d3936a9b9ba94e6d07f7e5b",
+    "url": "/poftfolio-page/static/media/IMG13.e9704e6e.jpg"
+  },
+  {
+    "revision": "8208c4a673761b72d46c17aa5af8c6e9",
+    "url": "/poftfolio-page/static/media/IMG14.8208c4a6.jpg"
+  },
+  {
+    "revision": "bc4130abae450cffda7b6c97ba285d0c",
+    "url": "/poftfolio-page/static/media/IMG15.bc4130ab.jpg"
+  },
+  {
+    "revision": "c46a48f6388c3fd74d43089b379be6a9",
+    "url": "/poftfolio-page/static/media/IMG2.c46a48f6.jpg"
+  },
+  {
+    "revision": "fa4437c0665d609bf43d3d315fab5391",
+    "url": "/poftfolio-page/static/media/IMG3.fa4437c0.jpg"
+  },
+  {
+    "revision": "a54d9715e2d1eea5c249efea5399a2d2",
+    "url": "/poftfolio-page/static/media/IMG4.a54d9715.jpg"
+  },
+  {
+    "revision": "50d6064d72d01489411c6093314cc200",
+    "url": "/poftfolio-page/static/media/IMG5.50d6064d.jpg"
+  },
+  {
+    "revision": "a0148c9b5af4fe96fc398b8138920379",
+    "url": "/poftfolio-page/static/media/IMG6.a0148c9b.jpg"
+  },
+  {
+    "revision": "38c2daedd153140795af8160ce0908fb",
+    "url": "/poftfolio-page/static/media/IMG7.38c2daed.jpg"
+  },
+  {
+    "revision": "48348542a325031226f246a83b4a1ecf",
+    "url": "/poftfolio-page/static/media/IMG8.48348542.jpg"
+  },
+  {
+    "revision": "6abbdbb00465b648afe9ad3be144a066",
+    "url": "/poftfolio-page/static/media/IMG9.6abbdbb0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "semmeljochen.github.io/portfolio-page/precache-manifest.9275c9bb0a3fb9a4adfb25e33ea7c04e.js"
+  "/poftfolio-page/precache-manifest.46819e200354ea1c88455ec3410164e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("semmeljochen.github.io/portfolio-page/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/poftfolio-page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
